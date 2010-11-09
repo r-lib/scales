e40b402c3fc5e90315325c7ccab82d06e7f20394
hadley <h.wickham@gmail.com>
2010-11-09 07:17:43 -0600

    Some examples and bug fixes for gradients

--- R/pal-gradient.r
+++ R/pal-gradient.r
@@ -1,18 +1,29 @@
 #' Colour gradient palette (continous).
 #' 
 #' @export
-gradient_n_pal <- function(colours, values = NULL, space="Lab") {
-  if (!is.null(values)) {
-    xs <- seq(0, 1, length = length(values))      
-    f <- approxfun(values, xs)
-    x <- f(x)
-  }
-
+gradient_n_pal <- function(colours, values = NULL, space = "Lab") {
   ramp <- colorRamp(colours, space = space)
-  function(x) rgb(ramp(x), max = 255)
+
+  function(x) {
+    if (!is.null(values)) {
+      xs <- seq(0, 1, length = length(values))      
+      f <- approxfun(values, xs)
+      x <- f(x)
+    }
+    
+    rgb(ramp(x), max = 255)
+  }
 }
 
-gradient_2_pal <- function(low = mnsl("10B 4/6"), mid = mnsl("N 4/0"), high = mnsl("10R 4/6"), space = "Lab") {
+#' @examples
+#' x <- seq(-1, 1, length = 100)
+#' r <- sqrt(outer(x^2, x^2, "+"))
+#' image(r, col = gradient_2_pal()(seq(0, 1, length = 12)))
+#' image(r, col = gradient_2_pal()(seq(0, 1, length = 30)))
+#' image(r, col = gradient_2_pal()(seq(0, 1, length = 100)))
+#' image(r, col = gradient_2_pal(low = 
+#'    mnsl(complement("10R 4/6", fix = T)))(seq(0, 1, length = 100)))
+gradient_2_pal <- function(low = mnsl("10B 4/6"), mid = mnsl("N 8/0"), high = mnsl("10R 4/6"), space = "Lab") {
   gradient_n_pal(c(low, mid, high), space = space)
 }
 
