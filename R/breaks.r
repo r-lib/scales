c949782403c042401a9aa173afeea27796448d85
hadley <h.wickham@gmail.com>
2010-11-11 08:53:46 -0600

    More checks for label format

--- R/breaks.r
+++ R/breaks.r
@@ -48,7 +48,9 @@ trans_breaks <- function(trans, n = 5, ...) {
 #'   will make a vector of breaks when given the range of the data
 #' @param labels either a vector of labels (character vector or list of 
 #'   expression) or a format function that will make a vector of labels when
-#'   called with a vector of breaks
+#'   called with a vector of breaks. Labels can only be specified manually if
+#'   breaks are - it is extremely dangerous to supply labels if you don't know
+#'   what the breaks will be.
 #' @examples
 #' cbreaks(c(0, 100))
 #' cbreaks(c(0, 100), pretty_breaks(3))
@@ -61,10 +63,21 @@ trans_breaks <- function(trans, n = 5, ...) {
 #' cbreaks(c(0, 1e6), labels = comma_format())
 #' cbreaks(c(0, 1e6), labels = dollar_format())
 #' cbreaks(c(0, 30), labels = dollar_format())
+#'
+#' # You can also specify them manually:
+#' cbreaks(c(0, 100), breaks = c(15, 20, 80))
+#' cbreaks(c(0, 100), breaks = c(15, 20, 80), labels = c(1.5, 2.0, 8.0))
+#' cbreaks(c(0, 100), breaks = c(15, 20, 80), 
+#'   labels = expression(alpha, beta, gamma))
 cbreaks <- function(range, breaks = pretty_breaks(), labels = scientific_format()) {
   
   if (is.function(breaks)) {
     breaks <- breaks(range)
+
+    if (!is.function(labels)) {
+      stop("Labels can only be manually specified in conjunction with breaks", 
+        call. = FALSE)
+    }
   }
   
   if (is.function(labels)) {
@@ -72,6 +85,11 @@ cbreaks <- function(range, breaks = pretty_breaks(), labels = scientific_format(
   } else {
     if (length(labels) != length(breaks)) {
       stop("Labels and breaks must be same length")
+    }
+    if (is.expression(labels)) {
+      labels <- as.list(labels)
+    } else {
+      labels <- as.character(labels)
     }
   }
   
