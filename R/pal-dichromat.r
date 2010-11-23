6ec32c3b5be75216703dbc3de5fd9d32fef178cb
hadley <h.wickham@gmail.com>
2010-11-23 16:28:07 -0600

    List dichromat scales in help

--- R/pal-dichromat.r
+++ R/pal-dichromat.r
@@ -1,6 +1,7 @@
 #' Dichromat (colour-blind) palette (discrete).
 #'
-#' @param name Name of colour palette.
+#' @param name Name of colour palette.  One of:
+#'   \Sexpr[results=rd]{scales:::dichromat_schemes()}
 #' @importFrom dichromat colorschemes
 #' @export
 #' @examples
@@ -17,4 +18,9 @@ dichromat_pal <- function(name) {
   
   pal <- colorschemes[[name]]
   function(n) pal[seq_len(n)]
+}
+
+
+dichromat_schemes <- function() {
+  str_c("\\code{", names(colorschemes), "}", collapse = ", ")
 }
