80c27b879b4a93ae14d845fa8482c1f4a287c502
hadley <h.wickham@gmail.com>
2010-12-24 08:11:21 -0600

    Change from alpha and size palettes to common rescale palette

--- R/pal-area.r
+++ R/pal-area.r
@@ -1,12 +1,3 @@
-#' Point size palette (continuous).
-#' 
-#' @param range Numeric vector of length two, giving range of possible sizes.
-#'   Should be greater than 0.
-#' @export
-size_pal <- function(range = c(1, 6)) {
-  function(x) rescale(x, range, c(0, 1))
-}
-
 #' Point area palette (continuous).
 #'
 #' @param range Numeric vector of length two, giving range of possible sizes.
---
+++ R/pal-rescale.r
@@ -0,0 +1,13 @@
+#' Rescale palette (continuous).
+#'
+#' Just rescales the input to the specific output range.  Useful for
+#' alpha, size, and continuous position.
+#'
+#' @param range Numeric vector of length two, giving range of possible
+#'   values.  Should be between 0 and 1.
+#' @export
+rescale_pal <- function(range = c(0.1, 1)) {
+  function(x) {
+    rescale(x, range, c(0, 1))
+  }
+}
