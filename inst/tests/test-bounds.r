0243e366ad99b427169391c8d7cea34d62ca9899
hadley <h.wickham@gmail.com>
2011-02-06 12:52:44 -0600

    A couple of tests for zero range inputs

--- inst/tests/test-bounds.r
+++ inst/tests/test-bounds.r
@@ -10,3 +10,9 @@ test_that("rescale_mid returns correct results", {
   expect_equal(rescale_mid(x, mid = 1, to = c(0, 10)), c(0, 2.5, 5))
   expect_equal(rescale_mid(x, mid = 1, to = c(8, 10)), c(8, 8.5, 9))  
 })
+
+
+test_that("zero range inputs return mid range", {
+  expect_that(rescale(0), equals(0.5))
+  expect_that(rescale(c(0, 0)), equals(c(0.5, 0.5)))  
+})
