a99daf9bb7119abea67dc3a31c3b7171a9e1a1b4
hadley <h.wickham@gmail.com>
2011-07-13 16:50:42 -0500

    More dynamic documentation tweaks

--- R/documentation.r
+++ R/documentation.r
@@ -1,12 +1,24 @@
 # Functions used for producing Rd chunks to reduce duplication in
 # documentation
 
+#' Dynamic documentation functions.
+#' 
+#' @name dyn-doc
+#' @keywords internal
+NULL
+
+#' @rdname dyn-doc
+#' @export
 seealso <- function(pattern) {
   require("scales")
   names <- ls("package:scales", pattern = pattern)
-  str_c("\\code{\\link{", names, "}}", collapse = ", ")
-  
+  str_c("\\code{\\link{", names, "}}", collapse = ", ")  
 }
 
+#' @rdname dyn-doc
+#' @export
 seealso_trans <- function() seealso("_trans$")
+
+#' @rdname dyn-doc
+#' @export
 seealso_pal <- function() seealso("_pal$")
