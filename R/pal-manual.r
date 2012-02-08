5c740627141988569da29ed2cf419c7a0f7abb4c
Winston Chang <winston@stdout.org>
2012-02-07 23:26:56 -0600

    Add limits argument to palette functions

--- R/pal-manual.r
+++ R/pal-manual.r
@@ -3,5 +3,12 @@
 #' @param values vector of values to be used as a palette.
 #' @export
 manual_pal <- function(values) {
-  function(n) values[seq_len(n)]
+  function(n = NULL, limits = NULL) {
+    if (!is.null(limits))
+      values[limits]
+    else if (!is.null(n))
+      values[seq_len(n)]
+    else
+      stop("Palette needs either n or limits specified.")
+  }
 }
