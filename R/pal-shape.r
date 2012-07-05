fe3a34a96f73f349a6a2b71161cae4ebf138475c
hadley <h.wickham@gmail.com>
2012-07-05 09:27:02 -0700

    Warning on more than 6 shapes. Fixes #16

--- R/pal-shape.r
+++ R/pal-shape.r
@@ -4,6 +4,14 @@
 #' @export
 shape_pal <- function(solid = TRUE) {
   function(n) {
+    if (n > 6) {
+      msg <- paste("The shape palette can deal with a maximum of 6 discrete ", 
+        "values because more than 6 becomes difficult to discriminate; ",
+        "you have ", n, ". Consider specifying shapes manually. if you ",
+        "must have them.", sep = "")
+      warning(paste(strwrap(msg), collapse = "\n"), call. = FALSE)
+    }
+    
     if (solid) {
       c(16, 17, 15, 3, 7, 8)[seq_len(n)]
     } else {
