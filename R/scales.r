9a198ae65bea81b143714487a6889c8a3f6b8fb9
hadley <h.wickham@gmail.com>
2015-08-24 11:23:36 -0500

    Minimise namespace again (after bad merge resolution)

--- R/scales.r
+++ R/scales.r
@@ -3,8 +3,8 @@
 #' @docType package
 #' @name package-scales
 #' @aliases scales package-scales
-#' @import munsell plyr Rcpp methods
 #' @useDynLib scales
+#' @importFrom methods setRefClass
 #' @importFrom plyr round_any is.discrete
 #' @importFrom Rcpp evalCpp
 NULL
