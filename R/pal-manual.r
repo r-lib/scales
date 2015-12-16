e3132da12b4e9e931e6ec55cf331c5d6510b05ae
jrnold <jeffrey.arnold@gmail.com>
2015-12-15 19:17:54 -0800

    Add warning for manual_pal
    Function returned by `manual_pal` gives a warning if n is greater than the number of values in the palette.

--- R/pal-manual.r
+++ R/pal-manual.r
@@ -3,5 +3,12 @@
 #' @param values vector of values to be used as a palette.
 #' @export
 manual_pal <- function(values) {
-  function(n) values[seq_len(n)]
+  function(n) {
+    n_values <- length(values)
+    if (n > n_values) {
+      warning(sprintf(paste("n = %d is greater than the number of values",
+                            "in the pallette (%d)", n , n_values)))
+    }
+    values[seq_len(n)]
+  }
 }
