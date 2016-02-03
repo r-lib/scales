63e0b50bc1d01fc7724efebaf9100d571fc5b77b
jrnold <jeffrey.arnold@gmail.com>
2016-02-03 12:01:58 -0800

    Fix typo

--- R/pal-manual.r
+++ R/pal-manual.r
@@ -7,7 +7,7 @@ manual_pal <- function(values) {
     n_values <- length(values)
     if (n > n_values) {
       warning(sprintf(paste("n = %d is greater than the number of values",
-                            "in the pallette (%d)", n , n_values)))
+                            "in the palette (%d)", n , n_values)))
     }
     values[seq_len(n)]
   }
