04bc6bd9903b89a1782a7e90c68aeb53f9100435
Kara Woo <woo.kara@gmail.com>
2017-06-28 15:56:18 -0700

    Fix typo in reverse_trans

--- R/trans-numeric.r
+++ R/trans-numeric.r
@@ -115,7 +115,7 @@ reverse_trans <- function() {
     "reverse",
     function(x) -x,
     function(x) -x,
-    minor.breaks = even_minor_breaks(reverse = TRUE))
+    minor_breaks = even_minor_breaks(reverse = TRUE))
 }
 
 #' Square-root transformation.
