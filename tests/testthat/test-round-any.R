cf1f24b5461a8d8caaa73f0b7cea10a89c0cf068
Dana Seidel <dpseidel@berkeley.edu>
2018-06-06 12:08:32 -0700

    Split round_any tests for numeric and POSIXct, tidy

--- tests/testthat/test-round-any.R
+++ tests/testthat/test-round-any.R
@@ -1,7 +1,6 @@
 context("round_any")
 
-test_that("round_any() function", {
-
+test_that("round_any function rounds numeric", {
   expect_equal(round_any(135, 10), 140)
   expect_equal(round_any(135, 100), 100)
   expect_equal(round_any(135, 25), 125)
@@ -11,12 +10,19 @@ test_that("round_any() function", {
   expect_equal(round_any(135, 10, ceiling), 140)
   expect_equal(round_any(135, 100, ceiling), 200)
   expect_equal(round_any(135, 25, ceiling), 150)
+})
 
-  expect_equal(round_any(as.POSIXct("2000-01-01 11:00:00", tz="UTC"), 86400),
-               as.POSIXct("2000-01-01", tz="UTC"))
-  expect_equal(round_any(as.POSIXct("2000-01-01 11:11:11", tz="UTC"), 3600),
-               as.POSIXct("2000-01-01 11:00", tz="UTC"))
-  expect_equal(round_any(as.POSIXct("2000-01-01 11:11:11", tz="UTC"), 10, ceiling),
-               as.POSIXct("2000-01-01 11:11:20", tz="UTC"))
-
+test_that("round_any() function rounds POSIXct", {
+  expect_equal(
+    round_any(as.POSIXct("2000-01-01 11:00:00", tz = "UTC"), 86400),
+    as.POSIXct("2000-01-01", tz = "UTC")
+  )
+  expect_equal(
+    round_any(as.POSIXct("2000-01-01 11:11:11", tz = "UTC"), 3600),
+    as.POSIXct("2000-01-01 11:00", tz = "UTC")
+  )
+  expect_equal(
+    round_any(as.POSIXct("2000-01-01 11:11:11", tz = "UTC"), 10, ceiling),
+    as.POSIXct("2000-01-01 11:11:20", tz = "UTC")
+  )
 })
