1664294e5c01308199c28a160f4793212c640bd8
Dana Seidel <dpseidel@berkeley.edu>
2018-08-17 13:38:52 -0700

    Check for dichromat package in .Rd build
    Fixes #172

--- R/pal-dichromat.r
+++ R/pal-dichromat.r
@@ -1,15 +1,17 @@
 #' Dichromat (colour-blind) palette (discrete).
 #'
 #' @param name Name of colour palette.  One of:
-#'   \Sexpr[results=rd,stage=build]{scales:::dichromat_schemes()}
+#'   \Sexpr[results=rd,stage=build]{if (requireNamespace("dichromat", quietly = TRUE)){scales:::dichromat_schemes()}}
 #' @export
 #' @examples
+#' if (requireNamespace("dichromat", quietly = TRUE)) {
 #' show_col(dichromat_pal("BluetoOrange.10")(10))
 #' show_col(dichromat_pal("BluetoOrange.10")(5))
 #'
 #' # Can use with gradient_n to create a continous gradient
 #' cols <- dichromat_pal("DarkRedtoBlue.12")(12)
 #' show_col(gradient_n_pal(cols)(seq(0, 1, length.out = 30)))
+#' }
 dichromat_pal <- function(name) {
   if (!requireNamespace("dichromat", quietly = TRUE)) {
     stop("Package dichromat must be installed for this function to work. Please install it.",
