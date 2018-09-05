ad49ecd53976c5ef696f1fc3e2c9fbb0250cbcb7
Mikko Marttila <mikmart@mikkomarttila.com>
2018-09-05 21:40:42 +0300

    Revert documenting

--- R/data.r
+++
@@ -1,6 +0,0 @@
-#' Metric prefixes used in the SI system
-#'
-#' Names, symbols and powers of metric prefixes in the SI system. Taken from
-#' \url{https://en.wikipedia.org/wiki/Metric_prefix}.
-#'
-"si_prefixes"
