5bf21f909c6a0a30f6d8d8d370ce8384878b9881
Mikko Marttila <mikmart@mikkomarttila.com>
2018-09-05 22:16:56 +0300

    Write micro as \u00b5

--- R/si-prefixes.R
+++ R/si-prefixes.R
@@ -4,7 +4,7 @@ si_prefixes <- data.frame(
            "deci", "centi", "milli", "micro", "nano",
            "pico", "femto", "atto", "zepto", "yocto"),
   symbol = c("Y", "Z", "E", "P", "T", "G", "M", "k", "h", "da", "",
-             "d", "c", "m", "µ", "n", "p", "f", "a", "z", "y"),
+             "d", "c", "m", "\u00b5", "n", "p", "f", "a", "z", "y"),
   power = c(24L, 21L, 18L, 15L, 12L, 9L, 6L, 3L, 2L, 1L, 0L,
             -1L, -2L, -3L, -6L, -9L, -12L, -15L, -18L, -21L, -24L),
   stringsAsFactors = FALSE
