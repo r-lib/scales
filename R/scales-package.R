32b98e7095571031ca78b73af9ce7876d3eda5cc
Hadley Wickham <h.wickham@gmail.com>
2019-10-26 07:24:11 -0500

    use_package_doc()

---
+++ R/scales-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+#' @useDynLib scales
+#' @importFrom Rcpp evalCpp
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
--- R/scales.r
+++
@@ -1,8 +0,0 @@
-#' Generic plot scaling methods
-#'
-#' @docType package
-#' @name package-scales
-#' @aliases scales package-scales
-#' @useDynLib scales
-#' @importFrom Rcpp evalCpp
-NULL
