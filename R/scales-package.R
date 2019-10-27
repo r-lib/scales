c5e69ac8819e0060138a70a4fe9711f5d960cc9a
Hadley Wickham <h.wickham@gmail.com>
2019-10-27 15:48:23 -0500

    use_lifecycle()

--- R/scales-package.R
+++ R/scales-package.R
@@ -6,5 +6,6 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom lifecycle deprecate_soft
 ## usethis namespace: end
 NULL
