e34a492ab773324c6f73b6b5c553ec23d7188c7a
Hadley Wickham <h.wickham@gmail.com>
2019-10-27 16:25:25 -0500

    Fix broken examples

--- R/breaks.r
+++ R/breaks.r
@@ -16,12 +16,12 @@
 #' demo_continuous(c(0, 100), breaks = breaks_width(20, 4))
 #'
 #' # This is also useful for dates
-#' one_months <- as.POSIXct(c("2020-05-01", "2020-06-01"))
-#' demo_datetime(two_months)
-#' demo_datetime(two_months, breaks = breaks_width("1 week"))
-#' demo_datetime(two_months, breaks = breaks_width("5 days"))
+#' one_month <- as.POSIXct(c("2020-05-01", "2020-06-01"))
+#' demo_datetime(one_month)
+#' demo_datetime(one_month, breaks = breaks_width("1 week"))
+#' demo_datetime(one_month, breaks = breaks_width("5 days"))
 #' # This is so useful that scale_x_datetime() has a shorthand:
-#' demo_datetime(two_months, date_breaks = "5 days")
+#' demo_datetime(one_month, date_breaks = "5 days")
 breaks_width <- function(width, offset = 0) {
   force_all(width, offset)
 
@@ -68,9 +68,9 @@ extended_breaks <- function(n = 5, ...) {
 #' @keywords internal
 #' @export
 #' @examples
-#' one_months <- as.POSIXct(c("2020-05-01", "2020-06-01"))
-#' demo_datetime(two_months)
-#' demo_datetime(two_months, breaks = pretty_breaks(10))
+#' one_month <- as.POSIXct(c("2020-05-01", "2020-06-01"))
+#' demo_datetime(one_month)
+#' demo_datetime(one_month, breaks = pretty_breaks(10))
 pretty_breaks <- function(n = 5, ...) {
   force_all(n, ...)
   n_default <- n
