0a94a31cd569fa5a9ef28a866463ef92f791f4e7
Hadley Wickham <h.wickham@gmail.com>
2019-10-29 15:32:31 -0500

    scientific_format -> label_scientific

---
+++ R/label-scientific.R
@@ -0,0 +1,50 @@
+#' Label numbers with scientific notation
+#'
+#' @section Old interface:
+#' `scientific_format()` and `scientific()` are retired; please use
+#' `label_scientific()`.
+#' @inherit number_format return params
+#' @param digits Number of digits to show before exponent.
+#' @param prefix,suffix Symbols to display before and after value.
+#' @param ... Other arguments passed on to [base::format()].
+#' @export
+#' @examples
+#' demo_continuous(c(1, 10))
+#' demo_continuous(c(1, 10), labels = label_scientific())
+#' demo_continuous(c(1, 10), labels = label_scientific(digits = 3))
+label_scientific <- function(digits = 3, scale = 1, prefix = "", suffix = "",
+                             decimal.mark = ".", trim = TRUE, ...) {
+  force_all(digits, scale, prefix, suffix, decimal.mark, trim, ...)
+  function(x) scientific(
+      x,
+      digits = digits,
+      scale = scale,
+      prefix = prefix,
+      suffix = suffix,
+      decimal.mark = decimal.mark,
+      ...
+    )
+}
+
+#' @export
+#' @rdname label_scientific
+scientific_format <- label_scientific
+
+#' @export
+#' @rdname label_scientific
+scientific <- function(x, digits = 3, scale = 1, prefix = "", suffix = "",
+                       decimal.mark = ".", trim = TRUE, ...) {
+  if (length(x) == 0) return(character())
+  x <- signif(x * scale, digits)
+  ret <- paste0(
+    prefix,
+    format(x, decimal.mark = decimal.mark, trim = trim, scientific = TRUE, ...),
+    suffix
+  )
+
+  # restore NAs from input vector
+  ret[is.na(x)] <- NA
+  names(ret) <- names(x)
+
+  ret
+}
