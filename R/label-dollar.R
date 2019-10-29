39e8f75ca6a57e05564277243108f047fc528cef
Hadley Wickham <h.wickham@gmail.com>
2019-10-29 07:16:37 -0500

    Add label_dollar()
    And improve docs

--- R/label-dollar.R
+++ R/label-dollar.R
@@ -1,44 +1,38 @@
 #' Currency formatter: round to nearest cent and display dollar sign
 #'
-#' The returned function will format a vector of values as currency.
-#' If `accuracy` is not specified, values are rounded to the nearest cent,
-#' and cents are displayed if any of the values has a non-zero cents and
-#' the largest value is less than `largest_with_cents` which by default
-#' is 100,000.
+#' Format numbers as currency, rounding values to dollars or cents using
+#' a convenient heuristic.
 #'
-#' @return A function with single parameter `x`, a numeric vector, that
-#'   returns a character vector.
-#' @param accuracy Number to round to, `NULL` for automatic guess.
-#' @param scale A scaling factor: `x` will be multiply by `scale` before
-#'   formating (useful to display the data on another scale, e.g. in k$).
+#' @section Old interface:
+#' `dollar()` and `format_dollar()` are retired; please use `label_dollar()`
+#' instead.
+#'
+#' @inherit number_format return params
+#' @param accuracy,largest_with_cents Number to round to. If `NULL`, the default,
+#'   values will be rounded to the nearest integer, unless any of the
+#'   values has non-zero fractional component (e.g. cents) and the largest
+#'   value is less than `largest_with_cents` which by default is 100,000.
 #' @param prefix,suffix Symbols to display before and after value.
-#' @param big.mark Character used between every 3 digits to separate thousands.
-#' @param decimal.mark The character to be used to indicate the numeric
-#'   decimal point.
-#' @param trim Logical, if `FALSE`, values are right-justified to a common
-#'   width (see [base::format()]).
-#' @param largest_with_cents The value that all values of `x` must
-#'   be less than in order for the cents to be displayed.
-#' @param negative_parens Should negative values be shown with parentheses?
+#' @param negative_parens Display negative using parentheses?
 #' @param ... Other arguments passed on to [base::format()].
-#' @param x A numeric vector to format.
 #' @export
 #' @examples
-#' dollar_format()(c(-100, 0.23, 1.456565, 2e3))
-#' dollar_format()(c(1:10 * 10))
-#' dollar(c(100, 0.23, 1.456565, 2e3))
-#' dollar(c(1:10 * 10))
-#' dollar(10^(1:8))
+#' demo_continuous(c(0, 1), labels = label_dollar())
+#' demo_continuous(c(1, 100), labels = label_dollar())
 #'
-#' usd <- dollar_format(prefix = "USD ")
-#' usd(c(100, -100))
+#' # Customise currency display with prefix and suffix
+#' demo_continuous(c(1, 100), labels = label_dollar(prefix = "USD "))
+#' euro <- dollar_format(
+#'   prefix = "",
+#'   suffix = "\u20ac",
+#'   big.mark = ".",
+#'   decimal.mark = ","
+#' )
+#' demo_continuous(c(1000, 1100), labels = euro)
 #'
-#' euro <- dollar_format(prefix = "", suffix = "\u20ac")
-#' euro(100)
-#'
-#' finance <- dollar_format(negative_parens = TRUE)
-#' finance(c(-100, 100))
-dollar_format <- function(accuracy = NULL, scale = 1, prefix = "$",
+#' # Use negative_parens = TRUE for finance style display
+#' demo_continuous(c(-100, 100), labels = label_dollar(negative_parens = TRUE))
+label_dollar <- function(accuracy = NULL, scale = 1, prefix = "$",
                           suffix = "", big.mark = ",", decimal.mark = ".",
                           trim = TRUE, largest_with_cents = 100000,
                           negative_parens = FALSE, ...) {
@@ -82,7 +76,12 @@ needs_cents <- function(x, threshold) {
 }
 
 #' @export
-#' @rdname dollar_format
+#' @rdname label_dollar
+dollar_format <- label_dollar
+
+#' @export
+#' @rdname label_dollar
+#' @param x A numeric vector
 dollar <- function(x, accuracy = NULL, scale = 1, prefix = "$",
                    suffix = "", big.mark = ",", decimal.mark = ".",
                    trim = TRUE, largest_with_cents = 100000,
