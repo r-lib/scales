ba1b2dea439c301f2415302a87e9ad0a9d607dae
David Hall <david.hall.physics@gmail.com>
2020-02-20 12:01:52 -0800

    Add billion_scale argument to label_dollar()

--- R/label-dollar.R
+++ R/label-dollar.R
@@ -14,6 +14,11 @@
 #'   value is less than `largest_with_cents` which by default is 100,000.
 #' @param prefix,suffix Symbols to display before and after value.
 #' @param negative_parens Display negative using parentheses?
+#' @param billion_scale Named list indicating suffixes given to large values
+#'   (e.g. thousands, millions, billions, trillions). Name gives suffix, and
+#'   value specifies the power-of-ten. The two most common scales are provided
+#'   (`billion_short()` and `billion_long()`). If `NULL`, the default, these
+#'   suffixes aren't used.
 #' @param ... Other arguments passed on to [base::format()].
 #' @export
 #' @family labels for continuous scales
@@ -33,10 +38,23 @@
 #'
 #' # Use negative_parens = TRUE for finance style display
 #' demo_continuous(c(-100, 100), labels = label_dollar(negative_parens = TRUE))
+#'
+#' # In English-speaking world, short scale is most prevalent
+#' dollar <- label_dollar(billion_scale = billion_short())
+#' demo_log10(c(1, 1e18), breaks = log_breaks(7, 1e3), labels = dollar)
+#'
+#' # Elsewhere, long scale is often used
+#' euro <- label_dollar(prefix = "", suffix = "EUR", billion_scale = billion_long())
+#' demo_log10(c(1, 1e18), breaks = log_breaks(7, 1e3), labels = euro)
+#'
+#' # You can also define a custom billion scale
+#' custom_dollar <- label_dollar(billion_scale = c(k = 3, mn = 6, bn = 9, tn = 12))
+#' demo_log10(c(1, 1e18), breaks = log_breaks(7, 1e3), labels = custom_dollar)
 label_dollar <- function(accuracy = NULL, scale = 1, prefix = "$",
                           suffix = "", big.mark = ",", decimal.mark = ".",
                           trim = TRUE, largest_with_cents = 100000,
-                          negative_parens = FALSE, ...) {
+                          negative_parens = FALSE, billion_scale = NULL,
+                          ...) {
   force_all(
     accuracy,
     scale,
@@ -47,6 +65,7 @@ label_dollar <- function(accuracy = NULL, scale = 1, prefix = "$",
     trim,
     largest_with_cents,
     negative_parens,
+    billion_scale,
     ...
   )
   function(x) dollar(
@@ -60,6 +79,7 @@ label_dollar <- function(accuracy = NULL, scale = 1, prefix = "$",
       trim = trim,
       largest_with_cents = largest_with_cents,
       negative_parens,
+      billion_scale = billion_scale,
       ...
     )
 }
@@ -86,7 +106,8 @@ dollar_format <- label_dollar
 dollar <- function(x, accuracy = NULL, scale = 1, prefix = "$",
                    suffix = "", big.mark = ",", decimal.mark = ".",
                    trim = TRUE, largest_with_cents = 100000,
-                   negative_parens = FALSE, ...) {
+                   negative_parens = FALSE, billion_scale = NULL,
+                   ...) {
   if (length(x) == 0) return(character())
   if (is.null(accuracy)) {
     if (needs_cents(x * scale, largest_with_cents)) {
@@ -101,6 +122,24 @@ dollar <- function(x, accuracy = NULL, scale = 1, prefix = "$",
 
   negative <- !is.na(x) & x < 0
   x <- abs(x)
+
+  if (!is.null(billion_scale)) {
+    breaks <- c(0, 10^billion_scale)
+
+    bn_suffix <- cut(abs(x),
+      breaks = c(unname(breaks), Inf),
+      labels = c(names(breaks)),
+      right = FALSE
+    )
+    bn_suffix[is.na(bn_suffix)] <- ""
+
+    sep <- if (suffix == "") "" else " "
+    suffix <- paste0(bn_suffix, sep, suffix)
+
+    scale <- unname(1 / breaks[bn_suffix])
+    # for handling Inf and 0-1 correctly
+    scale[which(scale %in% c(Inf, NA))] <- 1
+  }
 
   amount <- number(
     x,
@@ -125,4 +164,16 @@ dollar <- function(x, accuracy = NULL, scale = 1, prefix = "$",
   names(amount) <- names(x)
 
   amount
+}
+
+#' @export
+#' @rdname label_dollar
+billion_short <- function() {
+  c(K = 3, M = 6, B = 9, T = 12)
+}
+
+#' @export
+#' @rdname label_dollar
+billion_long <- function() {
+  c(K = 3, M = 6, B = 12, T = 18)
 }
--- tests/testthat/test-label-dollar.R
+++ tests/testthat/test-label-dollar.R
@@ -17,3 +17,20 @@ test_that("preserves names", {
 test_that("decimal.mark could be modified", {
   expect_equal(label_dollar(decimal.mark = ",")(123.45), "$123,45")
 })
+
+test_that("billion_scale works", {
+  x <- 10^(seq(0, 18, 3))
+
+  expect_equal(
+    label_dollar(billion_scale = billion_short())(x),
+    c("$1", "$1K", "$1M", "$1B", "$1T", "$1,000T", "$1,000,000T")
+  )
+  expect_equal(
+    label_dollar(billion_scale = billion_long())(x),
+    c("$1", "$1K", "$1M", "$1,000M", "$1B", "$1,000B", "$1T")
+  )
+  expect_equal(
+    label_dollar(billion_scale = c(k = 3, mn = 6, bn = 9, tn = 12))(x),
+    c("$1", "$1k", "$1mn", "$1bn", "$1tn", "$1,000tn", "$1,000,000tn")
+  )
+})
