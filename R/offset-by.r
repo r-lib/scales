7ad414889fcfdf6a9a67fc613e094205d1763662
Duncan Garmonsway <nacnudus@gmail.com>
2020-04-06 16:50:43 +0100

    Don't export offset_by()

--- R/offset-by.r
+++ R/offset-by.r
@@ -4,25 +4,21 @@
 #' @param size interval size
 #' @param ... other arguments passed on to methods
 #' @keywords internal
-#' @export
 #' @seealso [fullseq()]
 offset_by <- function(x, size, ...) UseMethod("offset_by")
 
-#' @export
 offset_by.numeric <- function(x, size, ...) {
   fun <- function(x)  seq(x, length.out = 2, by = size)[2]
   out <- lapply(x, fun)
   do.call(c, out)
 }
 
-#' @export
 offset_by.Date <- function(x, size, ...) {
   fun <- function(x) seq(x, length.out = 2, by = size)[2]
   out <- lapply(x, fun)
   do.call(c, out)
 }
 
-#' @export
 offset_by.POSIXt <- function(x, size, ...) {
   # for subsecond interval support
   # seq() does not support partial secs in character strings
