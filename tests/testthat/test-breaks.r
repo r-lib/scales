2ea58a1c5604287e539dfeff30dab7775dff5a0e
Brendan Hogan <bhogan@mitre.org>
2020-04-12 23:22:17 -0400

    copy tests from deprecated date_breaks to use breaks_width

--- tests/testthat/test-breaks.r
+++ tests/testthat/test-breaks.r
@@ -25,3 +25,24 @@ test_that("breaks_pretty() arguments are forcely evaluated on each call #81", {
   expect_equal(subfun2(1), subfuns[[2]](1))
 })
 
+test_that("breaks_width() works with POSIXct", {
+  times <- as.POSIXct(c("2000-01-01 08:29:58", "2000-01-01 08:30:10"), tz = "UTC")
+
+  expect_equal(
+    breaks_width("1 hour")(times),
+    as.POSIXct(c("2000-01-01 8:00:00 UTC", "2000-01-01 9:00:00 UTC"), tz = "UTC")
+  )
+  expect_equal(
+    breaks_width(".5 secs")(times)[1:2],
+    as.POSIXct(c("2000-01-01 08:29:58.0 UTC", "2000-01-01 08:29:58.5 UTC"), tz = "UTC")
+  )
+})
+
+test_that("breaks_width() works with Date", {
+  dates <- as.Date("2012-01-01") + 1:30
+  expect_equal(
+    breaks_width("1 month")(dates),
+    as.Date(c("2012-01-01", "2012-02-01"))
+  )
+})
+
