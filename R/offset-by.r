289f224bc332d478e0865f86d0cb03f62daaa83e
Duncan Garmonsway <nacnudus@gmail.com>
2020-05-09 23:33:27 +0100

    Remove dots from offset_by() args
    Unwittingly copied from fullseq()

--- R/offset-by.r
+++ R/offset-by.r
@@ -1,23 +1,23 @@
-offset_by <- function(x, size, ...) {
+offset_by <- function(x, size) {
   UseMethod("offset_by")
 }
 
 #' @export
-offset_by.numeric <- function(x, size, ...) {
   fun <- function(x)  seq(x, length.out = 2, by = size)[2]
   out <- lapply(x, fun)
   do.call(c, out)
+offset_by.numeric <- function(x, size) {
 }
 
 #' @export
-offset_by.Date <- function(x, size, ...) {
+offset_by.Date <- function(x, size) {
   fun <- function(x) seq(x, length.out = 2, by = size)[2]
   out <- lapply(x, fun)
   do.call(c, out)
 }
 
 #' @export
-offset_by.POSIXt <- function(x, size, ...) {
+offset_by.POSIXt <- function(x, size) {
   # for subsecond interval support
   # seq() does not support partial secs in character strings
   if (is.character(size) && (parsed <- parse_unit_spec(size))$unit == "sec") {
