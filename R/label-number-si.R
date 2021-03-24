92a5ba808629d4998550532691cfd09a5f2d4629
David Hall <david.hall.physics@gmail.com>
2021-03-24 07:25:06 -0700

    Move SI prefixes into SI file

--- R/label-number-si.R
+++ R/label-number-si.R
@@ -35,3 +35,15 @@ label_number_si <- function(accuracy = NULL, unit = NULL, sep = NULL, ...) {
     )
   }
 }
+
+# power-of-ten prefixes used by the International System of Units (SI)
+# https://www.bipm.org/en/measurement-units/prefixes.html
+#
+# note: irregular prefixes (hecto, deca, deci, centi) are not stored
+# because they don't commonly appear in scientific usage anymore
+si_powers <- (-8:8) * 3
+names(si_powers) <- c(
+  rev(c("m", "\u00b5", "n", "p", "f", "a", "z", "y")), "",
+  "k", "M",      "G", "T", "P", "E", "Z", "Y"
+)
+si_powers
