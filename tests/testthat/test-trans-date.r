3122324aa2484c6c55fed725d4c0149b8baa378c
Hadley Wickham <h.wickham@gmail.com>
2022-03-21 17:30:59 -0500

    Fix partial match warnings

--- tests/testthat/test-trans-date.r
+++ tests/testthat/test-trans-date.r
@@ -10,22 +10,22 @@ with_tz <- function(x, value) {
 
 test_that("date/time scales raise error on incorrect inputs", {
   time <- time_trans()
-  expect_error(time$trans(a_date), "Invalid input")
+  expect_error(time$transform(a_date), "Invalid input")
 
   date <- date_trans()
-  expect_error(date$trans(a_time), "Invalid input")
+  expect_error(date$transform(a_time), "Invalid input")
 })
 
 test_that("time scales learn timezones", {
   skip_if_not(getRversion() > "3.3.3")
   time <- time_trans()
-  x <- time$inv(time$trans(a_time))
+  x <- time$inverse(time$transform(a_time))
 
   expect_equal(tz(x), "UTC")
   expect_equal(tz2(x), "UTC")
 
   time <- time_trans()
-  x <- time$inv(time$trans(with_tz(a_time, "GMT")))
+  x <- time$inverse(time$transform(with_tz(a_time, "GMT")))
 
   expect_equal(tz(x), "GMT")
   expect_equal(tz2(x), "GMT")
@@ -33,7 +33,7 @@ test_that("time scales learn timezones", {
 
 test_that("tz arugment overrules default time zone", {
   time <- time_trans("GMT")
-  x <- time$inv(time$trans(a_time))
+  x <- time$inverse(time$transform(a_time))
 
   expect_equal(tz(x), "GMT")
   expect_equal(tz2(x), "GMT")
