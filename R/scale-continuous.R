636b1ca1ac494c368dd22ed21fad181f0b969017
Teun van den Brand <49372158+teunbrand@users.noreply.github.com>
2023-11-01 11:10:58 +0100

    `train_continuous()` coerces `new` to `numeric` (#374)

--- R/scale-continuous.R
+++ R/scale-continuous.R
@@ -46,7 +46,10 @@ train_continuous <- function(new, existing = NULL) {
     cli::cli_abort("Discrete value supplied to a continuous scale")
   }
 
-  suppressWarnings(range(existing, new, na.rm = TRUE, finite = TRUE))
+  # Needs casting to numeric because some `new` vectors can misbehave when
+  # combined with a NULL `existing` (#369)
+  suppressWarnings(range(existing, as.numeric(new),
+                         na.rm = TRUE, finite = TRUE))
 }
 
 # Map values for a continuous palette.
