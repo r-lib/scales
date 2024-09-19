076a01524ff8c981dfc7f5b470fbdbcf47cce378
Teun van den Brand <tahvdbrand@gmail.com>
2024-09-19 10:41:14 +0200

    add test

---
+++ tests/testthat/test-palette-registry.R
@@ -0,0 +1,27 @@
+test_that("palette getters and setters work as intended", {
+
+  # Test that palettes have been populated in .onLoad
+  expect_in(c("hue", "grey"), palette_names())
+
+  # We cannot get unknown palettes
+  expect_error(get_palette("rgb"), "Unknown palette")
+
+  # We cannot set nonsense palettes
+  expect_error(
+    set_palette("foobar", list(a = 1:2, b = "A")),
+    "must be a"
+  )
+
+  # Test we can set custom palettes
+  colours <- c("red", "green", 'blue')
+  set_palette("rgb", palette = colours)
+  expect_in("rgb", palette_names())
+
+  # Test we can get custom palettes
+  pal <- get_palette("rgb")
+  expect_equal(pal(length(colours)), colours)
+
+  # Test we can reset palettes
+  reset_palettes()
+  expect_false("rgb" %in% palette_names())
+})
