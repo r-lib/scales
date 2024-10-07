1679a57b23028acb863c92f2606a76342aae59e0
Teun van den Brand <tahvdbrand@gmail.com>
2024-10-07 16:57:41 +0200

    document

--- R/label-lut.R
+++ R/label-lut.R
@@ -1,4 +1,35 @@
 
+#' Labels from lookup tables
+#'
+#' Use `label_lut()` for looking up succinct breaks in a named character vector
+#' giving complete labels.
+#'
+#' @param lut A named character vector of labels. The names are expected to
+#'   match the breaks, and the values become the labels.
+#' @param nomatch A string to label breaks that do not match any name in `lut`.
+#'  When `NULL` (default), the breaks are not translated but are kept as-is.
+#'
+#' @return A labeller function that takes a character vector of breaks and
+#'   returns a character vector of labels.
+#' @export
+#' @family labels for discrete scales
+#' @examples
+#' # Example lookup table
+#' lut <- c(
+#'   "4" = "four wheel drive",
+#'   "r" = "rear wheel drive",
+#'   "f" = "front wheel drive"
+#' )
+#'
+#' # Typical usage
+#' demo_discrete(c("4", "r", "f"), labels = label_lut(lut))
+#' # By default, extra values ('w') will remain as-is
+#' demo_discrete(c("4", "r", "f", "w"), labels = label_lut(lut))
+#' # Alternatively, you can relabel extra values
+#' demo_discrete(
+#'   c("4", "r", "f", "w"),
+#'   labels = label_lut(lut, nomatch = "unknown")
+#' )
 label_lut <- function(lut = character(), nomatch = NULL) {
 
   if (!is.character(lut)) {
