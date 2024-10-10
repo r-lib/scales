40171c37845ae55e0880faf19d980a8b41e31182
Teun van den Brand <tahvdbrand@gmail.com>
2024-10-10 11:13:54 +0200

    add common <scales_pal> class

--- R/pal-.R
+++ R/pal-.R
@@ -65,7 +65,7 @@ new_continuous_palette <- function(fun, type, na_safe = NA) {
   if (!is.function(fun)) {
     cli::cli_abort("{.arg fun} must be a function.")
   }
-  class(fun) <- union("pal_continuous", class(fun))
+  class(fun) <- union(c("pal_continuous", "scales_pal"), class(fun))
   attr(fun, "type") <- type
   attr(fun, "na_safe") <- na_safe
   fun
@@ -77,7 +77,7 @@ new_discrete_palette <- function(fun, type, nlevels = NA) {
   if (!is.function(fun)) {
     cli::cli_abort("{.arg fun} must be a function.")
   }
-  class(fun) <- union("pal_discrete", class(fun))
+  class(fun) <- union(c("pal_discrete", "scales_pal"), class(fun))
   attr(fun, "type") <- type
   attr(fun, "nlevels") <- nlevels
   fun
