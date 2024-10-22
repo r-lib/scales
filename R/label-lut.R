29751e890024ce600d3eceb83e60c3ce6b6b0d13
Teun van den Brand <tahvdbrand@gmail.com>
2024-10-22 09:33:46 +0200

    rename to `label_dictionary()`

--- R/label-lut.R
+++ R/label-lut.R
@@ -1,13 +1,14 @@
 
 #' Labels from lookup tables
 #'
-#' Use `label_lut()` for looking up succinct breaks in a named character vector
-#' giving complete labels.
+#' Use `label_dictionary()` for looking up succinct breaks in a named character
+#' vector giving complete labels.
 #'
-#' @param lut A named character vector of labels. The names are expected to
-#'   match the breaks, and the values become the labels.
-#' @param nomatch A string to label breaks that do not match any name in `lut`.
-#'  When `NULL` (default), the breaks are not translated but are kept as-is.
+#' @param dictionary A named character vector of labels. The names are expected
+#'   to match the breaks, and the values become the labels.
+#' @param nomatch A string to label breaks that do not match any name in
+#'  `dictionary`. When `NULL` (default), the breaks are not translated but are
+#'  kept as-is.
 #'
 #' @return A labeller function that takes a character vector of breaks and
 #'   returns a character vector of labels.
@@ -22,15 +23,15 @@
 #' )
 #'
 #' # Typical usage
-#' demo_discrete(c("4", "r", "f"), labels = label_lut(lut))
+#' demo_discrete(c("4", "r", "f"), labels = label_dictionary(lut))
 #' # By default, extra values ('w') will remain as-is
-#' demo_discrete(c("4", "r", "f", "w"), labels = label_lut(lut))
+#' demo_discrete(c("4", "r", "f", "w"), labels = label_dictionary(lut))
 #' # Alternatively, you can relabel extra values
 #' demo_discrete(
 #'   c("4", "r", "f", "w"),
-#'   labels = label_lut(lut, nomatch = "unknown")
+#'   labels = label_dictionary(lut, nomatch = "unknown")
 #' )
-label_lut <- function(lut = character(), nomatch = NULL) {
+label_dictionary <- function(dictionary = character(), nomatch = NULL) {
 
   if (!is.character(lut)) {
     cli::cli_abort("The {.arg lut} argument must be a character vector.")
